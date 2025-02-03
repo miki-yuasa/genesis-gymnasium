e2332d70eb869ba9a459f244cb38964a75937c09
Mikihisa Yuasa <myuasa2@illinois.edu>
2025-02-03 00:05:38 -0600

    test: change env init

--- tests/test_chase_env.py
+++ tests/test_chase_env.py
@@ -1,3 +1,4 @@
+import os
 import genesis as gs
 import imageio
 import pytest
@@ -5,21 +6,19 @@ import pytest
 from genesis_gymnasium.envs.chase.chase_env import ChaseEnv
 
 
-@pytest.fixture
-def chase_env():
-    env = ChaseEnv()
-    return env
+chase_env = ChaseEnv()
 
 
-def test_chase_env_init(chase_env):
+def test_chase_env_init():
     env = chase_env
     assert isinstance(env, ChaseEnv)
     assert isinstance(env.scene, gs.Scene)
     assert env.render_mode == "rgb_array"
 
 
-def test_chase_env_render(chase_env):
+def test_chase_env_render():
     image_save_path = "tests/out/test_chase_env_render.png"
     env = chase_env
     rgb_array, _, _, _ = env.render()
     imageio.imsave(image_save_path, rgb_array)
+    assert os.path.exists(image_save_path)
